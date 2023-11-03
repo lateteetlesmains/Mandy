9c2429cb03325b500a0c746c32440a4e741f23fe
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2023-11-03 17:01:35 +0100

    Update Programme_rocket_mandy.ino

--- Programme_rocket_mandy/Programme_rocket_mandy.ino
+++ Programme_rocket_mandy/Programme_rocket_mandy.ino
@@ -1,3 +1,5 @@
+// Nom du fichier : IR_LED_Control.ino
+
 #include <IRremote.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -8,26 +10,53 @@ decode_results results;
 #define PIN            D7
 #define NUMPIXELS      12
 
+bool rainbow_On=false;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// Variables pour le mode arc-en-ciel
-unsigned long previousMillis = 0;
-const long interval = 50;
-int currentPixel = 0;
-int rainbowStep = 5;
-int mode = 0;  // 0 pour aucun effet, 1 pour arc-en-ciel
+int mode = 0;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(D3, OUTPUT);
   digitalWrite(D3, LOW);
+  
   pinMode(D4, OUTPUT);
   digitalWrite(D4, HIGH);
 
-  irrecv.enableIRIn();
+  irrecv.enableIRIn(); 
   strip.begin();
   strip.show();
+
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0,0,255));
+    strip.show();
+    delay(50);
+  }
+}
+void rainbow(int wait) {
+  uint16_t i, j;
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 void loop() {
@@ -36,63 +65,74 @@ void loop() {
     Serial.println(lastOctets, HEX);
     irrecv.resume();
 
-    switch (lastOctets) {
-      case 0xA:
-        mode = 1;  // Activer le mode arc-en-ciel
-        break;
-      case 0xB:
-        mode = 0;  // Désactiver l'effet
-        colorWipe(strip.Color(255, 0, 0));  // Rouge
-        break;
-      case 0xC:
-        mode = 0;  // Désactiver l'effet
-        colorWipe(strip.Color(0, 0, 0));  // Éteindre les LEDs
-        break;
-      case 0xD:
-        mode = 0;  // Désactiver l'effet
-        colorWipe(strip.Color(0, 0, 255));  // Bleu
-        break;
+    if (lastOctets == 0xA) {
+      mode++;
+      if (mode > 4) {
+        mode = 0;
+      }
+
+      switch(mode) {
+        case 0:
+          for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,0,255)); 
+            strip.show();
+            delay(50);
+          }
+          break;
+        case 1:
+          for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(255,0,0)); 
+            strip.show();
+            delay(50);
+          }
+          break;
+        case 2:
+          for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(0,255,0)); 
+            strip.show();
+            delay(50);
+          }
+          break;
+        case 3:
+          for(int i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, strip.Color(255,255,255)); 
+            strip.show();
+            delay(50);
+          }
+          break;
+       
+      }
     }
-  }
-
-  if (mode == 1) {
-    updateRainbow();
-  }
-}
-
-void colorWipe(uint32_t c) {
-  for (uint16_t i = 0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(50);
-  }
-}
-
-void updateRainbow() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    if (currentPixel < strip.numPixels()) {
-      int color = Wheel(((currentPixel * rainbowStep) % 255));
-      strip.setPixelColor(currentPixel, color);
+    if (lastOctets == 0xB) {
+      // Augmente la luminosité
+      uint8_t brightness = strip.getBrightness();
+      brightness += 20;
+      if (brightness > 255) brightness = 255;
+      strip.setBrightness(brightness);
       strip.show();
-      currentPixel++;
-    } else {
-      currentPixel = 0;
     }
-    previousMillis = currentMillis;
+    if (lastOctets == 0xC) {
+      // Éteint les LEDs
+      for(int i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.Color(0,0,0));
+        strip.show();
+      }
+    }
+    if (lastOctets == 0xD) {
+       if (rainbow_On==false)
+       {
+        rainbow_On=true;
+       }else
+       {
+       rainbow_On=false;
+        }
+      
+    }
+   lastOctets =0xF;
   }
-}
-
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   delay(500);
+   if (rainbow_On==true)
+   {
+    rainbow(20);
+   }
 }
